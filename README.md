fdba1744d2d5b16d788f1efdff2649e13461e91b
TheScorpoi <sobral@ua.pt>
2022-10-16 20:51:49 +0100

    init work

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # aa-project01
+
+
+### Notes
+
+https://iq.opengenus.org/algorithm-to-find-cliques-of-a-given-size-k/
+https://www.geeksforgeeks.org/find-all-cliques-of-size-k-in-an-undirected-graph/
+https://en.wikipedia.org/wiki/Clique_problem
+https://medium.com/100-days-of-algorithms/day-64-k-clique-c03fdc565b1e
---
+++ main.py
@@ -0,0 +1,38 @@
+from itertools import combinations
+import networkx as nx
+
+def k_cliques(graph):
+    # 2-cliques
+    cliques = [{i, j} for i, j in graph.edges() if i != j]
+    k = 2
+
+    while cliques:
+        # result
+        yield k, cliques
+
+        # merge k-cliques into (k+1)-cliques
+        cliques_1 = set()
+        for u, v in combinations(cliques, 2):
+            w = u ^ v
+            if len(w) == 2 and graph.has_edge(*w):
+                cliques_1.add(tuple(u | w))
+
+        # remove duplicates
+        cliques = list(map(set, cliques_1))
+        k += 1
+
+
+def print_cliques(graph, size_k):
+    for k, cliques in k_cliques(graph):
+        info = k, len(cliques), cliques[:3]
+        print('%d-cliques = %d, %s.' % (k, len(cliques), cliques))
+
+
+if __name__ == '__main__':
+    nodes, edges = 6, 10
+    size_k = 3
+    graph = nx.Graph()
+    graph.add_nodes_from(range(nodes))
+    graph.add_edges_from(combinations(range(nodes), 2))
+
+    print_cliques(graph, size_k)
