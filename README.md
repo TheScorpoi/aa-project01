2f9318ad04b23527b2454f0e3cad342c6b9c0a58
TheScorpoi <sobral@ua.pt>
2022-10-19 23:06:03 +0100

    add relatorio and graph visualization

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ https://iq.opengenus.org/algorithm-to-find-cliques-of-a-given-size-k/
 https://www.geeksforgeeks.org/find-all-cliques-of-size-k-in-an-undirected-graph/
 https://en.wikipedia.org/wiki/Clique_problem
 https://medium.com/100-days-of-algorithms/day-64-k-clique-c03fdc565b1e
+
+
+
+## -- 
+https://github.com/OnurArdaB/k-clique
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from itertools import combinations
+import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
+import time
 
 def k_cliques(graph):
     # 2-cliques
@@ -21,18 +24,37 @@ def k_cliques(graph):
         cliques = list(map(set, cliques_1))
         k += 1
 
-
 def print_cliques(graph, size_k):
     for k, cliques in k_cliques(graph):
         info = k, len(cliques), cliques[:3]
-        print('%d-cliques = %d, %s.' % (k, len(cliques), cliques))
-
+        #print('%d-cliques = %d, %s.' % (k, len(cliques), cliques))
 
 if __name__ == '__main__':
-    nodes, edges = 6, 10
+    
+    start_1 = time.time()
+    nodes, edges = 200, 10
     size_k = 3
     graph = nx.Graph()
     graph.add_nodes_from(range(nodes))
     graph.add_edges_from(combinations(range(nodes), 2))
+    
+    maximal_cliques_nodes = list(nx.find_cliques(graph))
+    maximal_cliques_nodes = np.array(maximal_cliques_nodes, dtype=list)
+    #print(maximal_cliques_nodes)
+    
+    #pos = nx.spring_layout(graph)
+    #nx.draw_networkx_nodes(graph, pos)
+    #nx.draw_networkx_labels(graph, pos)
+    #nx.draw_networkx_edges(graph, pos, edge_color='r', arrows = True)
 
+    plt.show()
+    #finnish_1 = time.time()
+    #3delta_1 = finnish_1 - start_1
+    #print("Time: desenhar ", delta_1)
+
+    start_2 = time.time()
     print_cliques(graph, size_k)
+    finnish_2 = time.time()
+    delta_2 = finnish_2 - start_2
+
+    print("Time: algoritmo ", delta_2)
