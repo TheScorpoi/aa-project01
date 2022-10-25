6be592be51a6b7bbcb4058b69a7ffc0266517182
TheScorpoi <sobral@ua.pt>
2022-10-25 22:43:16 +0100

    ommit

---
+++ generate-graph.py
@@ -0,0 +1,12 @@
+import random
+
+
+random.seed(98491)
+
+
+#generate a graph
+
+
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import networkx as nx
 import numpy as np
 import time
 import random
+from prettytable import PrettyTable
 
 def k_cliques(graph):
     # 2-cliques
     cliques = [{i, j} for i, j in graph.edges() if i != j]
-    k = 2
+    k = 5
 
     while cliques:
         # result
@@ -29,23 +30,44 @@ def print_cliques(graph, size_k):
     for k, cliques in k_cliques(graph):
         info = k, len(cliques), cliques[:3]
         #print('%d-cliques = %d, %s.' % (k, len(cliques), cliques))
+        
+        
+    
+def print_results(graph) -> None:
+    table = PrettyTable()
+    table.field_names = ["Vertices", "Edges", "Number os basic Operations", "Time", "Number of Solutions"]
+    
+    with open('results.txt', 'w') as f:
+        for k, cliques in k_cliques(graph):
+            info = k, len(cliques), cliques[:3]
+            table.add_row([graph.number_of_nodes(), graph.number_of_edges(), graph.number_of_nodes() * graph.number_of_edges(), time.time(), len(cliques)])
+            #f.write('%d-cliques = %d, %s.' % (k, len(cliques), cliques))
+        f.write(str(table))
 
 if __name__ == '__main__':
     
-    start_1 = time.time()
-    nodes, edges = 200, 10
+    #start_1 = time.time()
+    nodes, edges = 6, 4
     size_k = 3
-    graph = nx.Graph()
+    graph = nx.Graph(seed=98491)
     graph.add_nodes_from(range(nodes))
     graph.add_edges_from(combinations(range(nodes), 2))
     
-    maximal_cliques_nodes = list(nx.find_cliques(graph))
-    maximal_cliques_nodes = np.array(maximal_cliques_nodes, dtype=list)
+    #ad_matrix = nx.to_numpy_matrix(graph)
+    #print(ad_matrix)
+    
+    print(graph.nodes())
+    print(graph.edges())
+    
+    #maximal_cliques_nodes = list(nx.find_cliques(graph))
+    #maximal_cliques_nodes = np.array(maximal_cliques_nodes, dtype=list)
     #print(maximal_cliques_nodes)
     
     
-    random.seed(98491)
-    print(random.random())
+    #random.seed(98491)
+    #print(random.random())
+    
+    print_results(graph)
     
     #pos = nx.spring_layout(graph)
     #nx.draw_networkx_nodes(graph, pos)
