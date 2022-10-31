c19676b99ff36be9ee024af52e3f37e23a751def
TheScorpoi <sobral@ua.pt>
2022-10-31 01:56:14 +0000

    add new recursive BF algorithm and try some greedies

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ https://medium.com/100-days-of-algorithms/day-64-k-clique-c03fdc565b1e
 
 ## -- 
 https://github.com/OnurArdaB/k-clique
+
+https://iq.opengenus.org/greedy-approach-to-find-single-maximal-clique/
+http://www.stat.yale.edu/~yw562/teaching/684/lec02.pdf
+https://github.com/OnurArdaB/k-clique/blob/master/ProjectReport.
+https://arxiv.org/pdf/1808.09531.pdf
+
+https://en.wikipedia.org/wiki/Bron–Kerbosch_algorithm
--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+from pydoc import cli
 from vertex import Vertex
 from point import Point
 import random
@@ -7,38 +8,53 @@ import matplotlib.pyplot as plt
 from itertools import combinations
 from prettytable import PrettyTable
 import argparse
+import collections
+
 
 def generate_graph(vertexs_number: int, percentage: int):
     random.seed(98491)
-        
-    points = [(Point(random.randint(1,20), random.randint(1,20))) for i in range(vertexs_number)]
+
+    points = [(Point(random.randint(1, 20), random.randint(1, 20))) for i in range(vertexs_number)]
     vertexs = [(Vertex(i, points[i])) for i in range(vertexs_number)]
     vertex_label = [vertex.id for vertex in vertexs]
 
-    edges_number = int(max((vertexs_number*(vertexs_number-1)/2)*(percentage/100), vertexs_number-1))    
+    edges_number = int(max((vertexs_number*(vertexs_number-1)/2)*(percentage/100), vertexs_number-1))
     edges = set()
     visited = list()
+    adj_list = dict()
     unvisited = vertex_label
-    
+
     for i in range(edges_number):
-        if len(edges) == 0: #initial case: pick 2 random unvisited vertexes to form the first edge
+        if len(edges) == 0:  # initial case: pick 2 random unvisited vertexes to form the first edge
             two_random_vertexs = random.sample(unvisited, 2)
             edges.add(tuple(sorted(two_random_vertexs)))
             unvisited = list(set(unvisited) - set(two_random_vertexs))
             visited = list(set(visited).union(two_random_vertexs))
-        elif len(unvisited) != 0: #general case:
+        elif len(unvisited) != 0:  # general case:
             vertex_1 = random.choice(unvisited)
             vertex_2 = random.choice(visited)
-            edges.add(tuple(sorted([vertex_1,vertex_2])))
+            edges.add(tuple(sorted([vertex_1, vertex_2])))
             unvisited.remove(vertex_1)
         else:
-            edges.add(tuple(sorted(random.sample(vertex_label[:vertexs_number], 2))))
-            
-    return vertexs, edges
+            edges.add(
+                tuple(sorted(random.sample(vertex_label[:vertexs_number], 2))))
+    # generate adjacency list
+    for node1, node2 in sorted(edges):
+        if node1 in adj_list:
+            adj_list[node1].append(node2)
+        else:
+            adj_list[node1] = [node2]
+        if node2 in adj_list:
+            adj_list[node2].append(node1)
+        else:
+            adj_list[node2] = [node1]
 
-def k_cliques(graph):
+    return vertexs, edges, adj_list
+
+
+def k_cliques(graph, k):
     cliques = [{i, j} for i, j in graph.edges() if i != j]
-    k = 2  #initial k
+    k = k
     delta = 0
     number_of_solutions = 0
     number_of_basic_operations = 0
@@ -46,106 +62,169 @@ def k_cliques(graph):
     while cliques:
         # result
         yield k, cliques, delta, number_of_solutions, number_of_basic_operations
-        
+
         number_of_solutions = 0
         number_of_basic_operations = 0
-        
+
         initial = time.time()
         # merge k-cliques into (k+1)-cliques
         cliques_1 = set()
         for u, v in combinations(cliques, 2):
             number_of_basic_operations += 1
             w = u ^ v
+            number_of_solutions += 1
             if len(w) == 2 and graph.has_edge(*w):
                 cliques_1.add(tuple(u | w))
-                number_of_solutions += 1
+                # print("cliques_1: ", k ,"   ", cliques_1)
         # remove duplicates
         cliques = list(map(set, cliques_1))
         delta = time.time() - initial
         k += 1
-        
-def print_results_BF(graph) -> None:
+
+
+def print_results_BF(graph, k) -> None:
     table = PrettyTable()
-    table.field_names = ["k", "Number of Cliques", "Number os basic Operations", "Time", "Number of Solutions"]
-    
+    table.field_names = ["k", "Number of Cliques",
+        "Number os basic Operations", "Time", "Number of Solutions"]
+
     with open('results.txt', 'w') as f:
         i = 0
-        for k, cliques, delta, number_of_solutions, number_of_basic_operations in k_cliques(graph):
-            table.add_row([k, len(cliques), number_of_basic_operations, delta, number_of_solutions])
-            print(table)
-        i+=1
+        for k, cliques, delta, number_of_solutions, number_of_basic_operations in k_cliques(graph, k):
+            table.add_row(
+                [k, len(cliques), number_of_basic_operations, delta, number_of_solutions])
+        i += 1
         f.write(str(table))
+
 
 def plot_graph(e):
     G = nx.Graph()
     for edge in e:
-        G.add_edge(edge[0],edge[1])
-    nx.draw(G, node_size=700 , with_labels=True)
+        G.add_edge(edge[0], edge[1])
+    nx.draw(G, node_size=700, with_labels=True)
     plt.show()
-    
+
+
 def get_keys_array(dict):
     result = []
     for key in dict.keys():
         result.append(key)
     return result
 
-def p_clique (graph, k, v):
+
+def find_clique_greedy(graph):
+    clique = []
+    vertices = list(graph.keys())
+    rand = random.randrange(0, len(vertices), 1)
+    clique.append(vertices[rand])
+    # print("clique inicial: ", clique)
+    for v in vertices:
+        # print("v: ", v)
+        if v in clique:
+            continue
+        isNext = True
+        for u in clique:
+            if u in graph[v]:
+                continue
+            else:
+                isNext = False
+                break
+        if isNext:
+           clique.append(v)
+    return sorted(clique)
+
+
+def p_clique(graph, k):
     if k == 1:
         return True
     clique = []
-    vertices = v
-    max_ln=0
-    max_dict=[]
-    for i in range (0,len(graph)):
-        clique= []
-        clique.append (vertices [i])
+    vertices = get_keys_array(graph)
+    max_ln = 0
+    max_dict = []
+    for i in range(0, len(graph)):
+        clique = []
+        clique.append(vertices[i])
         for v in vertices:
             if v in clique:
                 continue
             isNext = True
             for u in clique:
-                if graph.has_edge(u,v):
-                    continue 
+                if u in graph[v]:
+                    continue
                 else:
                     isNext = False
                     break
             if isNext:
-                clique.append (v)
-                if k <= len (clique):
+                clique.append(v)
+                if k <= len(clique):
                     return True
-        if len (clique) > max_ln:
-            max_ln=len (clique)
-            max_dict=clique
-    if k <= len (clique):
-        print(clique)
+        if len(clique) > max_ln:
+            max_ln = len(clique)
+            max_dict = clique
+    if k <= len(clique):
         return True
     else:
-        print(clique)
         return False
+
+
+def cliques_recursive(neighbors, r, p, x):
+    if not p and not x:
+        yield r
+    else:
+        for v in min((p - neighbors[u] for u in p | x), key=len):
+            yield from cliques_recursive(
+                neighbors, r | {v}, p & neighbors[v], x & neighbors[v]
+            )
+            p.remove(v)
+            x.add(v)
+
+def cliques(graph):
+    neighbors = collections.defaultdict(set)
+    for v, n_v in graph.items():
+        for u in n_v:
+            if u != v:
+                neighbors[u].add(v)
+                neighbors[v].add(u)
+    yield from cliques_recursive(neighbors, set(), set(neighbors), set())
+    
+    
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser("K-Cliques Problem")
     parser.add_argument("-r", type=str, help="Resolution type: BF-Search or Heuristic")
-    parser.add_argument("-n", type=int, default=15, help="Number of vertexs, default=15")
-    parser.add_argument("-p", type=int, default=50, help="Percentage of edges, default=50, can be: 12.5, 25, 50, 75")
+    parser.add_argument("-n", type=int, default=45, help="Number of vertexs, default=15")
+    parser.add_argument("-p", type=int, default=75, help="Percentage of edges, default=50, can be: 12.5, 25, 50, 75")
+    parser.add_argument("-k", type=int, default=50, help="Percentage of edges, default=3, can be: 12.5, 25, 50, 75")
     parser.add_argument("-d", type=int, default=0, help="Draw the graph: 0 - No, 1 - Yes")
     parser.add_argument("-t", type=int)
     args = parser.parse_args()
     
     #create graph
     A = time.time()
-    v,e = generate_graph(args.n, args.p)
+    v,e, adj_list = generate_graph(args.n, args.p)
     graph = nx.Graph()
     for edge in e:
         graph.add_edge(edge[0],edge[1])
         
     if args.t == 1:
-        print(p_clique(graph, 2, v))
+        #print(find_clique_greedy(adj_list))
+        #print(p_clique(adj_list, 2))
+        #plot_graph(e)
+        #print(list(nx.find_cliques_recursive(graph)))
+        #plot_graph(e)
+        for clique in cliques(adj_list):
+            print(clique)
+            
+        plot_graph(e)
+        
+    
+    k = int(args.n * (args.k / 100))
+    print(k)
     
     if args.r == "BF-Search":
-        print_results_BF(graph)
+        print_results_BF(graph, k)
         print("Time: ", time.time() - A)
+        print(nx.cliques_containing_node(graph))
         if args.d == 1:
             plot_graph(e)
     
